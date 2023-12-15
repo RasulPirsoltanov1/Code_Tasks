1c6d1325f8282f50d7aa9ed6a7552a4678a08688
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-12-16 00:51:11 +0400

    Task_Week_2_Day_5

---
+++ SendGridMailTask/Loops_Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Loops_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 10;
+            //triangle
+            Console.WriteLine("\n Triangle \n");
+            string fiqure = "";
+            for (int i = 0; i < length / 2; i++)
+            {
+                Console.WriteLine(fiqure.PadLeft(i + 1, '*'));
+            }
+
+            //rectangle
+            Console.WriteLine("\n Rectangular \n");
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (i == 0 || i == length / 2 - 1)
+                {
+                    Console.WriteLine(fiqure.PadLeft(length, '*'));
+                }
+                else
+                {
+                    Console.WriteLine(fiqure.PadLeft(1, '*') + fiqure.PadRight(length - 2, ' ') + fiqure.PadRight(1, '*'));
+                }
+            }
+
+            Console.WriteLine("\n Square \n");
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                Console.WriteLine(fiqure.PadLeft(length, '*'));
+            }
+
+        }
+    }
+}
---
+++ SendGridMailTask/Vurma_Cedveli/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Vurma_Cedveli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                Vurma Cedveli\n");
+            int length = 10;
+
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j < length; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
+                }
+                if (i != length)
+                    Console.WriteLine("\n------------------------\n");
+            }
+        }
+
+    }
+}
