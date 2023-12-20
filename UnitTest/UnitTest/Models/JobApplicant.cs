5468d1b19b56965c1ff632d10552e4ff45e5ba7a
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-12-21 02:28:00 +0400

    UnitTest

---
+++ UnitTest/UnitTest/Models/JobApplicant.cs
@@ -0,0 +1,9 @@
+﻿namespace UnitTest.Models
+{
+    public class JobApplication
+    {
+        public Applicant Applicant { get; set; }
+        public int YerarsOfExperience { get; set; }
+        public List<string> TechStacList { get; set; }
+    }
+}
